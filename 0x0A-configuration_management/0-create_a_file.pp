32d677014630e7c2212681f8f7a3315496c22514
taiebchaabini <taiebchaabini@gmail.com>
2020-03-23 16:32:00 +0000

    Fixing syntax

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a new file Holberton in tmp directory
-file { '/tmp/holberton': 
-	ensure	=> present,
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	mode	=> '0744',
-	content => 'I love Puppet',
+file { '/tmp/holberton':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
