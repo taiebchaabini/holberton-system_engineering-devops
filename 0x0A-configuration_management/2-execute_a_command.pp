cbb8128c3370c361d3bde2afabf3589ebf7a086d
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 08:50:36 +0000

    Puppet-lint fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kill a process KILLMENOW
 exec { 'killmenow':
   command => 'pkill killmenow',
-  path     => '/usr/bin:/usr/sbin:/bin',
-  onlyif   => 'pgrep killmenow'
+  path    => '/usr/bin:/usr/sbin:/bin',
+  onlyif  => 'pgrep killmenow'
 }
