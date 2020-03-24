dcb299023a9363b00a299aee12d004f07e51ca38
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 15:31:11 +0000

    Changing stuff

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,21 @@
 # Configure ssh config for using custom private key and 
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => ' IdentityFile ~/.ssh/holberton',
-}
+
 file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => ' PasswordAuthentication no',
+  ensure   => 'present',
+  name     => 'AUTH_PWD',
+  path     => '/etc/ssh/ssh_config',
+  line     => '    PasswordAuthentication no',
+  match    => '#?   PasswordAuthentication yes',
+  replace  => true,
+  multiple => false,
 }
+file_line { 'Declare identity file':
+  ensure   => 'present',
+  name     => 'IDENTITY_FILE',
+  path     => '/etc/ssh/ssh_config',
+  line     => '    IdentityFile ~/.ssh/holberton',
+  match    => '#?   IdentityFile ~/.ssh/.+',
+  replace  => true,
+  multiple => false,
+}
+FILE_LINE['AUTH_PWD'] ~> FILE_LINE['IDENTITY_FILE']
