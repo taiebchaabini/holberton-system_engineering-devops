1d591d83255faa2c4bd183432befbdfd2bc80327
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 15:34:55 +0000

    Removing regex

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -4,18 +4,12 @@ file_line { 'Turn off passwd auth':
   ensure   => 'present',
   name     => 'AUTH_PWD',
   path     => '/etc/ssh/ssh_config',
-  line     => '    PasswordAuthentication no',
-  match    => '#?   PasswordAuthentication yes',
-  replace  => true,
-  multiple => false,
+  line     => 'PasswordAuthentication no',
 }
 file_line { 'Declare identity file':
   ensure   => 'present',
   name     => 'IDENTITY_FILE',
   path     => '/etc/ssh/ssh_config',
-  line     => '    IdentityFile ~/.ssh/holberton',
-  match    => '#?   IdentityFile ~/.ssh/.+',
-  replace  => true,
-  multiple => false,
+  line     => 'IdentityFile ~/.ssh/holberton',
 }
 FILE_LINE['AUTH_PWD'] ~> FILE_LINE['IDENTITY_FILE']
