273a846bf8742182bc51ce2ea88f7680adb1bde9
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 14:36:25 +0000

    Changing attributes

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,12 +2,11 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match   => '*PasswordAuthentication yes',
+  line   => ' PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
+  line   => ' IdentityFile ~/.ssh/holberton',
 }
