33433ebad068f5b1a2a2e20fa844aa6e6102e772
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 14:00:34 +0000

    Fix to file_line

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,6 +3,7 @@ file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
+  match   => '*PasswordAuthentication yes',
 }
 
 file_line { 'Declare identity file':
