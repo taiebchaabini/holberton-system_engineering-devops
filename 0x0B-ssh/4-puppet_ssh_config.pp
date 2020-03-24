6c098361ef00db653cd3f55d09de2de0ceb3c8e3
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 14:47:15 +0000

    Changing order

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 # Configure ssh config for using custom private key and 
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => ' PasswordAuthentication no',
-}
-
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => ' IdentityFile ~/.ssh/holberton',
+}
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => ' PasswordAuthentication no',
 }
