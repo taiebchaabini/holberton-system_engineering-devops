ce9607a1ae8e3bf5927395821e7ba8e865a79b71
taiebchaabini <taiebchaabini@gmail.com>
2020-03-24 09:15:18 +0000

    Fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kill a process KILLMENOW
 exec { 'killmenow':
-  command => 'pkill killmenow',
+  command => 'pkill -f killmenow',
   path    => '/usr/bin:/usr/sbin:/bin',
   onlyif  => 'pgrep killmenow'
 }
