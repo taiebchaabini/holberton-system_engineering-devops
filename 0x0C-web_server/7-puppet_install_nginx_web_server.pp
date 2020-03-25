feb892934e4e8912a943a9e5e9ce2ac5aa0985a6
taiebchaabini <taiebchaabini@gmail.com>
2020-03-25 20:52:30 +0000

    Adding notify / installing with package

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,22 @@
 #  Nginx web server (w/ Puppet) 
-exec { 'install nginx server':
-  name    => 'install_nginx',
-  command =>  'apt-get install nginx -y'
+package { 'nginx':
+  ensure =>  'installed',
+}
+
+service {'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
 }
 
 file { 'define index.html':
   ensure  => 'present',
-  name    => 'default_page',
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Holberton School'
 }
 
 file_line { 'perform a redirection':
   ensure => 'present',
-  name   => 'redirect_me',
-  line   => 'redirect ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => 'redirect ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  notify => Service['nginx']
 }
-
-EXEC['install_nginx'] ~> FILE['default_page'] ~> FILE_LINE['redirect_me']
