658fcb3c6336f04ab4703a94ec69b40a78b1e407
taiebchaabini <taiebchaabini@gmail.com>
2020-04-08 14:34:25 +0000

    Removing body output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -w '%{size_request}\n' $1
+curl -so /dev/null $1 -w '%{size_download}\n'
